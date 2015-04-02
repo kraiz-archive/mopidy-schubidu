f334e510f17be702741a228df64c4455fb09e91f
kraiz <lars.kreisz@gmail.com>
2015-04-02 21:50:46 +0200

    no config schema to test as we have no config values

--- tests/test_extension.py
+++ tests/test_extension.py
@@ -1,6 +1,6 @@
 from __future__ import unicode_literals
 
-from mopidy_gmui import Extension, frontend as frontend_lib
+from mopidy_gmui import Extension
 
 
 def test_get_default_config():
@@ -10,16 +10,3 @@ def test_get_default_config():
 
     assert '[gmui]' in config
     assert 'enabled = true' in config
-
-
-def test_get_config_schema():
-    ext = Extension()
-
-    schema = ext.get_config_schema()
-
-    # TODO Test the content of your config schema
-    #assert 'username' in schema
-    #assert 'password' in schema
-
-
-# TODO Write more tests
